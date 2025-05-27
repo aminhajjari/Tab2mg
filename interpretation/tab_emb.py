a00d55ddc03b775cbd6173c29eabae27b411cfa0
Seungeun Lee <77534726+duneag2@users.noreply.github.com>
2025-05-27 16:01:23 +0900

    Update tab_emb.py

--- interpretation/tab_emb.py
+++ interpretation/tab_emb.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.1
-#   kernelspec:
-#     display_name: PyTorch 2.2 (NGC 23.11/Python 3.10) on Backend.AI
-#     language: python
-#     name: python3
-# ---
-
-# +
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -23,8 +8,6 @@ import pandas as pd
 import os
 import argparse
 
-###########################변경 사항#############################
-num_classes = 2  # 클래스 개수 수정
 
 class TABMLP(nn.Module):
     def __init__(self, n_cont_features=8, tab_latent_size=12, num_classes=5):
@@ -37,18 +20,17 @@ class TABMLP(nn.Module):
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
-###########################변경 사항#############################
 
 def main():
-    # argparse로 외부 인자 받기
-    parser = argparse.ArgumentParser(description="Train a diabetes model with a CSV file.")
+    parser = argparse.ArgumentParser(description="Welcome to DualSHAP")
     parser.add_argument('--csv', required=True, help="Path to the input CSV file.")
     parser.add_argument('--csv_name', type=str, required=True, help='Path to the dataset (csv)')
+    parser.add_argument('--num_classes', type=int, required=True, help='# of classes')
     args = parser.parse_args()
 
+    num_classes = args.num_classes
     csv_file = args.csv
 
-    # CSV 파일 로드
     csv_name = args.csv_name
 
     df = pd.read_csv(csv_file)
@@ -61,26 +43,21 @@ def main():
         y = df.loc[:, target_col].values
         X = df.drop(target_col, axis=1).values
 
-    # Train/Test 데이터 분리
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    # 데이터 표준화
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
-
-    # PyTorch 텐서로 변환
+    
     X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
     X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
     y_train_tensor = torch.tensor(y_train, dtype=torch.long)  # CrossEntropyLoss requires `long`
     y_test_tensor = torch.tensor(y_test, dtype=torch.long)
 
-    # 모델 초기화
     n_cont_features = X.shape[1]
     tab_latent_size = n_cont_features + 4
     model = TABMLP(n_cont_features=n_cont_features, tab_latent_size=tab_latent_size, num_classes=num_classes)
 
-    # Loss와 Optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.01)
 
@@ -118,14 +95,14 @@ def main():
 
             if test_acc > best_acc:
                 best_acc = test_acc
-                # Best 모델 저장
+                
                 torch.save(model, best_model_path)
 
         if epoch % 10 == 0:
             print(f"Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.4f}, "
                   f"Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}")
 
-    # Best 모델 로드 및 평가
+
 #     best_model = TABMLP(tab_latent_size=tab_latent_size, num_classes=num_classes)
     best_model = torch.load(best_model_path)
     best_model.eval()
@@ -145,6 +122,5 @@ def main():
 if __name__ == "__main__":
     main()
 
-# -
 
 
